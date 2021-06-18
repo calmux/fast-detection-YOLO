b305012379f637b23db96f0044c4642535416f13
calmux <calmuxoto@proton.me>
2021-06-18 14:44:55 +0000

    crush: clean up types, const-ness

---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+#include "mainwindow.h"
+#include <QApplication>
+#include <QDesktopWidget>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    a.setWindowIcon(QIcon("../data/yolo.ico"));
