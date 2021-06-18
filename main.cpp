2b0702ebde7adcbd336fe522678cc2eecef95518
calmux <calmuxoto@proton.me>
2021-06-18 18:07:55 +0000

    apidoc

--- main.cpp
+++ main.cpp
@@ -7,3 +7,13 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon("../data/yolo.ico"));
+
+    int W = 500;
+    int H = 500;
+    int screenWidth;
+    int screenHeight;
+    int x, y;
+
+    QDesktopWidget desktop;
+
+    screenWidth = desktop.screen()->width();
