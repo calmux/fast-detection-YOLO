ac5466075e4ae79d9ad9a71d6c2cff266f753447
calmux <calmuxoto@proton.me>
2021-06-27 12:56:11 +0000

    removed temporary file

---
+++ mainwindow.cpp
@@ -0,0 +1,11 @@
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QtGui>
+#include <QtWidgets/QMessageBox>
+#include <QtWidgets/QFileDialog>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "yolo_v2_class.h"
+#include "thread"
