de4a93e08ed8f58f46f0917bfff6d5b05bb0fcc1
calmux <calmuxoto@proton.me>
2021-06-27 19:09:52 +0000

    renamed files to match folder name

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,3 +9,21 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "yolo_v2_class.h"
 #include "thread"
+
+using namespace std;
+using namespace cv;
+
+std::vector<std::string> objects_names_from_file(std::string const filename) {
+    std::ifstream file(filename);
+    std::vector<std::string> file_lines;
+    if (!file.is_open()) return file_lines;
+    for (std::string line; file >> line;) file_lines.push_back(line);
+    std::cout << "object names loaded \n";
+    return file_lines;
+}
+void draw_boxes(cv::Mat mat_img, std::vector<bbox_t> result_vec, std::vector<std::string> obj_names) {
+
+    for (auto &i : result_vec) {
+            cv::Scalar color(60, 160, 260);
+            cv::Scalar color2(120, 60, 150);
+            cv::rectangle(mat_img, cv::Rect(i.x, i.y, i.w, i.h), color, 3);
