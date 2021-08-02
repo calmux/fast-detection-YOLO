b6a1f0221302cae2ca547b14a5f11624c8a58eee
calmux <calmuxoto@proton.me>
2021-08-02 17:16:52 +0000

    fix dropdown

--- mainwindow.h
+++ mainwindow.h
@@ -14,3 +14,13 @@
 using namespace std;
 using namespace cv;
 namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
