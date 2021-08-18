2bab863a783e2385aff822953b49804692f5decb
calmux <calmuxoto@proton.me>
2021-08-18 18:35:41 +0000

    ejemplo

--- yolo_v2_class.h
+++ yolo_v2_class.h
@@ -1,2 +1,14 @@
 
 #pragma once
+#ifdef YOLODLL_EXPORTS
+#if defined(_MSC_VER)
+#define YOLODLL_API __declspec(dllexport) 
+#else
+#define YOLODLL_API __attribute__((visibility("default")))
+#endif
+#else
+#if defined(_MSC_VER)
+#define YOLODLL_API __declspec(dllimport) 
+#else
+#define YOLODLL_API
+#endif
