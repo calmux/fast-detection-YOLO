0e8a222df62724ddeaed20df2874e086ef00c24e
calmux <calmuxoto@proton.me>
2021-08-25 14:15:45 +0000

    HOTFIX: Migration to AndroidX (& minSDK 16)

--- yolo_v2_class.h
+++ yolo_v2_class.h
@@ -47,3 +47,15 @@ class Detector {
 	const int cur_gpu_id;
 public:
 	float nms = .4;
+	bool wait_stream;
+
+	YOLODLL_API Detector(std::string cfg_filename, std::string weight_filename, int gpu_id = 0);
+	YOLODLL_API ~Detector();
+
+	YOLODLL_API std::vector<bbox_t> detect(std::string image_filename, float thresh = 0.2, bool use_mean = false);
+	YOLODLL_API std::vector<bbox_t> detect(image_t img, float thresh = 0.2, bool use_mean = false);
+	static YOLODLL_API image_t load_image(std::string image_filename);
+	static YOLODLL_API void free_image(image_t m);
+	YOLODLL_API int get_net_width() const;
+	YOLODLL_API int get_net_height() const;
+	YOLODLL_API int get_net_color_depth() const;
